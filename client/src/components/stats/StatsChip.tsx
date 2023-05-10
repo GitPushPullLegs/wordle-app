093fe1a1303104fb1ee0a35c88cae3c4d2caa973
Joe Aguilar <Jose.aguilar.6694@gmail.com>
2023-05-10 13:11:22 -0700

    Made it clear that the stats chip can be opened. Fixed typo where the current streak was green if < max streak, its now current >= max.

--- client/src/components/stats/StatsChip.tsx
+++ client/src/components/stats/StatsChip.tsx
@@ -3,12 +3,13 @@ import {Avatar, Chip, Typography} from "@mui/material";
 import {UserContext} from "../../context/UserContext";
 import {grey} from "@mui/material/colors";
 import StatsDialog from "./StatsDialog";
+import {KeyboardArrowRight} from "@mui/icons-material";
 
 
 export default function StatsChip() {
   const { stats } = useContext(UserContext)
 
-  const newLongestStreak = (stats?.current_streak ?? 0) < (stats?.longest_streak ?? 1)
+  const newLongestStreak = (stats?.current_streak ?? 0) >= (stats?.longest_streak ?? 1)
 
   const [openDialog, setOpenDialog] = useState(false)
 
@@ -21,6 +22,8 @@ export default function StatsChip() {
       }
       label={"Current Streak"}
       onClick={() => setOpenDialog(true)}
+      onDelete={() => setOpenDialog(true)}
+      deleteIcon={<KeyboardArrowRight />}
     />
     <StatsDialog open={openDialog} onClose={() => setOpenDialog(false)} />
   </>)
