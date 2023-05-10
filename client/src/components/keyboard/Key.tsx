f9de2b1e0c5e53fe9ad1f7474a581b33413f7348
Joe Aguilar <Jose.aguilar.6694@gmail.com>
2023-05-10 10:02:07 -0700

    Made keys smaller to accomodate smaller screens.

--- client/src/components/keyboard/Key.tsx
+++ client/src/components/keyboard/Key.tsx
@@ -7,16 +7,20 @@ interface KeyProps {
   value: string
   onClick: (value: string) => void
   state: "in-word" | "not-in-word" | "unknown"
+  width?: number | string
 }
 
 
-export default function Key({ value, onClick, state }: KeyProps) {
+export default function Key({ value, onClick, state, width }: KeyProps) {
   return (
     <Button
       sx={{
         bgcolor: state !== "unknown" ? (state === "in-word" ? "success.main" : grey[800]) : grey[50],
         color: state !== "unknown" ? "white" : grey[800],
+        flex: 1,
+        ...(width ? { minWidth: width } : {})
       }}
+      size={"small"}
       onClick={() => onClick(value)}
     >
       {value}
--- client/src/components/keyboard/Keyboard.tsx
+++ client/src/components/keyboard/Keyboard.tsx
@@ -24,18 +24,18 @@ export default function Keyboard({ onClick, inWordKeys, notInWordKeys }: Keyboar
     >
       <Stack direction={"row"} spacing={1}>
         {["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"].map((v => (
-          <Key key={v} value={v} onClick={onClick} state={inWordKeys.includes(v) ? "in-word" : (notInWordKeys.includes(v) ? "not-in-word" : "unknown")} />
+          <Key key={v} value={v} onClick={onClick} state={inWordKeys.includes(v) ? "in-word" : (notInWordKeys.includes(v) ? "not-in-word" : "unknown")} width={"24px"} />
         )))}
       </Stack>
       <Stack direction={"row"} spacing={1}>
         {["A", "S", "D", "F", "G", "H", "J", "K", "L"].map((v => (
-          <Key key={v} value={v} onClick={onClick} state={inWordKeys.includes(v) ? "in-word" : (notInWordKeys.includes(v) ? "not-in-word" : "unknown")} />
+          <Key key={v} value={v} onClick={onClick} state={inWordKeys.includes(v) ? "in-word" : (notInWordKeys.includes(v) ? "not-in-word" : "unknown")} width={"24px"} />
         )))}
       </Stack>
       <Stack direction={"row"} spacing={1}>
         <Key value={"Enter"} onClick={onClick} state={"unknown"} />
         {["Z", "X", "C", "V", "B", "N", "M"].map((v => (
-          <Key key={v} value={v} onClick={onClick} state={inWordKeys.includes(v) ? "in-word" : (notInWordKeys.includes(v) ? "not-in-word" : "unknown")} />
+          <Key key={v} value={v} onClick={onClick} state={inWordKeys.includes(v) ? "in-word" : (notInWordKeys.includes(v) ? "not-in-word" : "unknown")} width={"24px"} />
         )))}
         <Key value={"Delete"} onClick={onClick} state={"unknown"} />
       </Stack>
