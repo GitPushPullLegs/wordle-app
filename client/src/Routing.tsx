5384619cf9707a1c8d6621ebbaa9e53d1464bc17
Joe Aguilar <jose@schoolytics.com>
2023-05-09 20:19:03 -0700

    Init login page

--- client/src/Routing.tsx
+++ client/src/Routing.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 export default function Routing() {
   return (
     <Routes>
-      <Route path={"/"} element={<>Hello World</>} />
+      <Route path={"/"} element={<LoginPage/>} />
     </Routes>
   )
 }
