e0601c9b986b4a2ba004ca121d8a301861deca87
Joe Aguilar <jose@schoolytics.com>
2023-05-09 20:27:07 -0700

    Created basic login card for login page. Will make it nicer once I get it up running correctly.

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,6 +1,10 @@
 import React, {useContext, useEffect} from "react";
 import {UserContext} from "../context/UserContext";
 import {useNavigate} from "react-router-dom";
+import {Card, Stack, Typography} from "@mui/material";
+import {grey} from "@mui/material/colors";
+import Logo from "../components/Logo";
+import {LoadingButton} from "@mui/lab";
 
 
 export default function LoginPage() {
@@ -15,8 +19,39 @@ export default function LoginPage() {
   }, [user])
 
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <Card
+      variant={"outlined"}
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        bgcolor: grey[50]
+      }}
+    >
+      <Stack
+        sx={{ p: 2 }}
+        spacing={2}
+        alignItems={"center"}
+      >
+        <Logo/>
+        <Typography variant={"h6"}>{`Ready to play?`}</Typography>
+        <LoadingButton
+          variant={"outlined"}
+          component={"a"}
+          href={"/api/auth/login"}
+          loading={fetchStatus === "fetching"}
+          startIcon={
+            <img
+              src={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"}
+              width={20}
+              alt={"Google sign-in"}
+            />}
+          sx={{ width: "max-content"}}
+        >
+          {`Continue with Google`}
+        </LoadingButton>
+      </Stack>
+    </Card>
   )
 }
