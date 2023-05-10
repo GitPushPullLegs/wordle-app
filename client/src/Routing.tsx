736cb5a5ecc287f83960980db2d9ec4b5f0d1eb2
Joe Aguilar <jose@schoolytics.com>
2023-05-09 20:35:06 -0700

    Created a PrivateRoute function to protect some routes (like the /play route). If the user is fetched it either redirects to the login page or the play page. If we're still waiting, it returns nothing.

--- client/src/Routing.tsx
+++ client/src/Routing.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import {Route, Routes} from "react-router-dom";
+import React, {useContext} from "react";
+import {Navigate, Outlet, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import {UserContext} from "./context/UserContext";
+
+
+function PrivateRoute() {
+  const { fetchStatus, user } = useContext(UserContext)
+
+  if (fetchStatus === "fetched" && user.user_id) {  // If fetched and user found, continue.
+    return <Outlet/>
+  } else if (fetchStatus === "fetched" || fetchStatus === "failed") {  // Else, redirect to log in.
+    return <Navigate to={"/"} />
+  } else {  // If still fetching.
+    return <></>
+  }
+}
 
 export default function Routing() {
   return (
     <Routes>
       <Route path={"/"} element={<LoginPage/>} />
+      <Route element={<PrivateRoute/>}>
+        <Route path={"/play"} element={<>Play</>} />
+      </Route>
     </Routes>
   )
 }
