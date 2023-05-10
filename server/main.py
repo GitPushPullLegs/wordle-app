8fe2bb5a16193ceeb0a83fe12dcb06bf3a02aabc
Joe Aguilar <jose@schoolytics.com>
2023-05-09 20:17:03 -0700

    Verify jwt and load user before each request.

--- server/main.py
+++ server/main.py
@@ -1,11 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
 
+from flask_jwt_extended.exceptions import JWTExtendedException
+from jwt import PyJWTError
+
+from server.models.user import User
+
 from server.secret_manager import Secrets
 
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, verify_jwt_in_request, get_jwt_identity
 from flask_openapi3 import OpenAPI, APIBlueprint
-from flask import jsonify
+from flask import jsonify, redirect, g
 
 from server.api.auth import api as auth_api
 from server.api.user import api as user_api
@@ -23,6 +28,26 @@ jwt = JWTManager(app)
 api = APIBlueprint("/api", __name__, url_prefix="/api")
 api.register_api(auth_api)
 api.register_api(user_api)
+
+
+@app.before_request
+def load_user():
+    g.current_user = None
+    try:
+        verify_jwt_in_request()
+        identity = get_jwt_identity()
+        if identity and identity.get("user_id"):
+            user = User.get(user_id=identity["user_id"])
+            if user:
+                g.current_user = user
+    except (JWTExtendedException, PyJWTError) as e:
+        print(e)
+        pass
+
+
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    return redirect("/")
 
 
 @api.get("/ping")
