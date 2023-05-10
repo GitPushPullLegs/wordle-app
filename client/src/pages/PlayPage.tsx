9f81e1b38002959f8b353dbae968c93869448cfa
Joe Aguilar <Jose.aguilar.6694@gmail.com>
2023-05-09 21:03:35 -0700

    Added TileGrid to PlayPage and created keypress listener.

--- client/src/pages/PlayPage.tsx
+++ client/src/pages/PlayPage.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
+import Navbar from "../components/Navbar";
+import {Card} from "@mui/material";
+import {grey} from "@mui/material/colors";
+import TileGrid from "../components/gameboard/TileGrid";
 
 
 export default function PlayPage() {
-  return (
-    <div>
-      Play Page
-    </div>
-  )
+
+  const [guesses, setGuesses] = useState<string[]>([])
+  const [currentGuess, setCurrentGuess] = useState("")
+
+  const handleKeyPress = useCallback((event: KeyboardEvent) => {
+    let key = event.key
+    if (key === "Backspace") {
+      setCurrentGuess(current => current.slice(0, -1))
+    } else if (/^[a-zA-Z]$/i.test(key)) {
+      setCurrentGuess(current => current.length >= 5 ? current : current + key.toUpperCase())
+    } else if (key === "Enter") {
+      if (currentGuess.length !== 5) {
+        console.log("Too short.") // TODO: Inform the user.
+      } else {
+        console.log("Submit the guess.")
+      }
+    }
+  }, [currentGuess, guesses])
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress)
+
+    return () => document.removeEventListener('keydown', handleKeyPress)
+  }, [handleKeyPress])
+
+  return (<>
+    <Navbar/>
+    <Card
+      variant={"outlined"}
+      sx={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        bgcolor: grey[50]
+      }}
+    >
+      <TileGrid guesses={guesses} currentGuess={currentGuess} answer={"123"} />
+    </Card>
+  </>)
 }
