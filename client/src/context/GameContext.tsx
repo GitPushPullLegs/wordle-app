b1b649f31d40d49e371bf470705bea7f1b9284ef
Joe Aguilar <Jose.aguilar.6694@gmail.com>
2023-05-09 21:41:58 -0700

    Filled out game functions.

--- client/src/context/GameContext.tsx
+++ client/src/context/GameContext.tsx
@@ -1,4 +1,5 @@
 import React, {createContext, useState} from "react";
+import useFetch from "../api/useFetch";
 
 
 interface Game {
@@ -6,15 +7,21 @@ interface Game {
   word: string
 }
 
+interface Guess {
+  guess: string
+}
+
 interface GameContextData {
   game?: Game
+  previousGuesses: Guess[]
   startGame: () => void
-  endGame: () => void
-  getPreviousGuesses: () => void
+  endGame: (solvedRow?: number) => void
+  getPreviousGuesses: (gameId: string) => void
   guess: (word: string) => void
 }
 
 export const GameContext = createContext<GameContextData>({
+  previousGuesses: [],
   startGame: () => {},
   endGame: () => {},
   getPreviousGuesses: () => {},
@@ -23,25 +30,59 @@ export const GameContext = createContext<GameContextData>({
 
 
 export function GameProvider({ children }: { children: React.ReactNode}) {
+  const fetch = useFetch()
+
   const [game, setGame] = useState<Game>()
+  const [previousGuesses, setPreviousGuesses] = useState<Guess[]>([])
+
   function startGame() {
-    console.log("start game")
+    fetch("/api/game/start")
+      .then((response: {status: string, game: Game, is_new: boolean}) => {
+        setGame(response.game)
+        return response
+      })
+      .then((response) => {
+        if (!response.is_new) {
+          getPreviousGuesses(response.game.game_id)
+        }
+      })
   }
 
-  function endGame() {
-    console.log("end game")
+  function endGame(solvedRow?: number) {
+    fetch("/api/game/stop", {
+      args: {
+        game_id: game?.game_id,
+        finished_at: new Date().toISOString(),
+        ...(solvedRow && { solved_row: solvedRow })
+      }
+    })
   }
 
-  function getPreviousGuesses() {
-    console.log("get previous guesses")
+  function getPreviousGuesses(gameId: string) {
+    fetch("/api/game/guess/list", {
+      args: {
+        game_id: gameId,
+      }
+    })
+      .then((response: { status: string, guess_list: Guess[] }) => {
+        setPreviousGuesses(response.guess_list)
+      })
   }
 
   function guess(word: string) {
-    console.log(word)
+    setPreviousGuesses(current => [...current, { guess: word }])
+
+    fetch("/api/game/guess/submit", {
+      args: {
+        game_id: game?.game_id,
+        guess: word,
+        correct: word === game?.word
+      }
+    })
   }
 
   return (
-    <GameContext.Provider value={{ game, startGame, endGame, getPreviousGuesses, guess }}>
+    <GameContext.Provider value={{ game, previousGuesses, startGame, endGame, getPreviousGuesses, guess }}>
       {children}
     </GameContext.Provider>
   )
