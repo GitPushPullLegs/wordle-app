cb6b0dc9ce9c6c7fbf63608ca9d59b3e5650a082
Joe Aguilar <jose@schoolytics.com>
2023-05-09 19:48:01 -0700

    Had chatGPT generate a list of 5-letter words for me. This works for now as an MVP.

---
+++ server/word_list.py
@@ -0,0 +1,14 @@
+WORDS = [
+    "audio", "baker", "chair", "dance", "eager", "fable", "grape", "hazel", "ideal", "jolly", "kneel", "lemon", "major",
+    "novel", "opera", "pilot", "quilt", "ruler", "silly", "teach", "urban", "vital", "waste", "xenon", "yield", "zebra",
+    "abuse", "bison", "cable", "dwarf", "erase", "fudge", "gloom", "hatch", "irate", "joker", "krill", "laser", "icing",
+    "motel", "noble", "oasis", "argue", "blaze", "crown", "drift", "elite", "frost", "grind", "honey", "image", "jolly",
+    "knock", "lucky", "merry", "ninja", "olive", "proud", "quake", "roast", "sweat", "tiger", "upset", "virus", "watch",
+    "xerox", "young", "zesty", "acute", "bliss", "clerk", "doubt", "erase", "flame", "ghost", "heart", "inner", "jumbo",
+    "kinky", "leash", "metal", "navel", "ankle", "blame", "chaos", "dream", "epoch", "frail", "gravy", "hurry", "ivory",
+    "joker", "knelt", "lunar", "mirth", "notch", "ovary", "plush", "query", "rhyme", "spoil", "trump", "uncle", "vocal",
+    "whale", "xylan", "yacht", "zooey", "abide", "bloom", "crave", "dwell", "enjoy", "fable", "grind", "hinge", "impel",
+    "jolly", "knick", "laser", "melon", "novel", "orbit", "plaid", "quote", "route", "silly", "toast", "urban", "vivid",
+    "wince", "xenon", "yield", "zesty", "apron", "brisk", "comet", "drone", "elbow", "frost", "gloom", "hatch", "irate",
+    "jolly", "knack", "lemon", "meant", "noble", "ocean", "pilot", "quiet", "ruler", "sting", "trick", "uncut", "viper",
+    "worry", "xerus", "yodel", "zonal", "awful", "blunt", "cower", "dodge", "eager", "flour", "gauze", "hoard"]
